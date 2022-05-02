11ff879ae1f9247159415e464505eab38e2318cc
Jonas <36059164+JonasLos@users.noreply.github.com>
2022-05-02 12:00:27 -0500

    Update converter.py
    added output into text file

--- converter.py
+++ converter.py
@@ -17,7 +17,9 @@ items = mydoc.getElementsByTagName('image')
 i = 0
 #go through all images, and find polylines (aka childNodes), extract points and convert to floats
 while i < items.length - 1:
-
+    image_name = items[i].attributes['name'].value #get image name
+    txt_file_name = image_name.replace('.jpg','.lines.txt') #get image name and convert to text file name
+    #print(txt_file_name)
     if items[i].childNodes.item(1).attributes['label'].value == 'line3':
         line3_points = items[i].childNodes.item(1).attributes['points'].value
         output_line3 = line3_points.replace(';',',')
@@ -40,18 +42,27 @@ while i < items.length - 1:
               x_desired=(j-b)/m
               coordinates1.append((x_desired,j))
 
-    print(coordinates1)
+    line_points1 = ' '.join(map(str,coordinates1))
+    line_points1 = line_points1.replace(') (',',')
+    line_points1 = line_points1.replace(')','')
+    line_points1 = line_points1.replace('(','')
+    line_points1 = line_points1.replace(', ',' ')
+    line_points1 = line_points1.replace(',',' ')
+    #print(line_points1)
+    with open(txt_file_name, 'w') as f: #write points to text file
+        f.write(line_points1)
+        f.write('\n')
 
     if  items[i].childNodes.length == 5:
         if items[i].childNodes.item(3).attributes['label'].value == 'line3':
             line3_points = items[i].childNodes.item(3).attributes['points'].value
             output_line3 = line3_points.replace(';',',')
-            x1, y1, x2, y2, x3, y3 = map( float, output_line3.split(',') )
+            x1, y1, x2, y2, x3, y3 = map(float, output_line3.split(',') )
 
         elif items[i].childNodes.item(3).attributes['label'].value == 'line2':
             line2_points = items[i].childNodes.item(3).attributes['points'].value
             output_line2 = line2_points.replace(';',',')
-            x1, y1, x2, y2, x3, y3 = map( float, output_line2.split(',') )
+            x1, y1, x2, y2, x3, y3 = map(float, output_line2.split(',') )
 
     coordinates2=[]
     for j in range(590, 300, -10):
@@ -63,6 +74,14 @@ while i < items.length - 1:
               m, b= func(x2,y2,x3,y3)
               x_desired=(j-b)/m
               coordinates2.append((x_desired,j)) 
-    print(coordinates1)
-   
+    line_points2 = ' '.join(map(str,coordinates2))
+    line_points2 = line_points2.replace(') (',',')
+    line_points2 = line_points2.replace(')','')
+    line_points2 = line_points2.replace('(','')
+    line_points2 = line_points2.replace(', ',' ')
+    line_points2 = line_points2.replace(',',' ')
+    #print(line_points2)
+    with open(txt_file_name, 'a') as f: #note a instead of w to append and not overwrite the previous points
+        f.write(line_points2)
+
     i = i + 1 
