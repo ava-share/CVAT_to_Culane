c842ada0343185b280eb1405da32962cb1aa93db
Jonas <36059164+JonasLos@users.noreply.github.com>
2022-05-02 23:01:26 -0500

    Update converter.py
    take in more than three points for polyline and add no-line condition

--- converter.py
+++ converter.py
@@ -1,13 +1,44 @@
+# Authors: Pardis Taghavi & Jonas Lossner
+# May 2022
+# This program converts annotations in 'CVAT for images' format to Culane Dataset compliant format
+# Input: annotations.xml - (name may differ) from CVAT
+# Output: frameXXXX.lines.txt - individual textfile for each image
+# Limitations: The code currently only handles 2 Lines, it can handle less than 2 lines but not more (this would be easy to add)
+
 from xml.dom import minidom
     
 def func(x,y,xp,yp):
-    if x1 != x2:
+    if x != xp:
         m=(yp-y)/(xp-x)
         b=y-m*x
     elif x==xp:
-        b=x1
-        m=9999
+        b=x
+        m=999999
     return m,b
+
+def func2(listOfCoef):
+  for n in range (0,len(listOfCoef),2):
+    x.append(float(listOfCoef[n]))
+  for m in range (1,len(listOfCoef),2):
+    y.append(float(listOfCoef[m]))
+  return x,y
+
+
+def finding_piecewise_equations(x,y):
+  coordinates=[]
+  CoefNumber=len(y) 
+  c=1
+  for j in range(590, 300, -10):
+    if c!=CoefNumber and len(y)!=0 :   
+      if j>y[c] and j<=y[c-1] :
+        m, b= func(x[c-1],y[c-1],x[c],y[c])
+        x_desired=(j-b)/m
+        coordinates.append((x_desired,j))
+      if j<= y[c]:
+        c+=1
+  return coordinates
+
+
 # parse an xml file by name
 mydoc = minidom.parse('annotations.xml')
 
@@ -17,65 +48,63 @@ items = mydoc.getElementsByTagName('image')
 i = 0
 #go through all images, and find polylines (aka childNodes), extract points and convert to floats
 while i < items.length - 1:
-    x1, y1, x2, y2, x3, y3 = 0, 0, 0, 0, 0, 0
+   
     image_name = items[i].attributes['name'].value #get image name
     txt_file_name = image_name.replace('.jpg','.lines.txt') #get image name and convert to text file name
     #print(txt_file_name)
-    if items[i].childNodes.item(1).attributes['label'].value == 'line3':
-        line3_points = items[i].childNodes.item(1).attributes['points'].value
-        output_line3 = line3_points.replace(';',',')
-        x1, y1, x2, y2, x3, y3 = map( float, output_line3.split(',') )
 
-    elif items[i].childNodes.item(1).attributes['label'].value == 'line2':
-        line2_points = items[i].childNodes.item(1).attributes['points'].value
-        output_line2 = line2_points.replace(';',',')
-        x1, y1, x2, y2, x3, y3 = map( float, output_line2.split(',') )
-    
-    coordinates1=[]
+    gListA=[]
+    gListB=[]
+    x=[]
+    y=[]
+    if items[i].childNodes.length >= 2: #if there are no lines there is still a length of 1 so if less than 2 then there are no lines and we create an empty file
 
-    for j in range(590, 300, -10):
-          if j>=y2 and j<=y1 :
-              m, b= func(x1,y1,x2,y2)
-              x_desired=(j-b)/m
-              coordinates1.append((x_desired,j))
-          elif j>=y3 and j<=y2 :
-              m, b= func(x2,y2,x3,y3)
-              x_desired=(j-b)/m
-              coordinates1.append((x_desired,j))
+        if items[i].childNodes.item(1).attributes['label'].value == 'line3':
+            line3_points = items[i].childNodes.item(1).attributes['points'].value
+            output_line3 = line3_points.replace(';',',')
+            gListA= output_line3.split(',')
 
-    line_points1 = ' '.join(map(str,coordinates1))
-    line_points1 = line_points1.replace(') (',',')
-    line_points1 = line_points1.replace(')','')
-    line_points1 = line_points1.replace('(','')
-    line_points1 = line_points1.replace(', ',' ')
-    line_points1 = line_points1.replace(',',' ')
-    #print(line_points1)
-    with open(txt_file_name, 'w') as f: #write points to text file
-        f.write(line_points1)
-        f.write('\n')
+        elif items[i].childNodes.item(1).attributes['label'].value == 'line2':
+            line2_points = items[i].childNodes.item(1).attributes['points'].value
+            output_line2 = line2_points.replace(';',',')
+            gListA= output_line2.split(',')
+        
+           
+        x,y = func2(gListA)
+        coordinates1=finding_piecewise_equations(x,y)
+        #print(coordinates1)
 
-    if  items[i].childNodes.length == 5:
-        x1, y1, x2, y2, x3, y3 = 0, 0, 0, 0, 0, 0
+        line_points1 = ' '.join(map(str,coordinates1))
+        line_points1 = line_points1.replace(') (',',')
+        line_points1 = line_points1.replace(')','')
+        line_points1 = line_points1.replace('(','')
+        line_points1 = line_points1.replace(', ',' ')
+        line_points1 = line_points1.replace(',',' ')
+        #print(line_points1)
+        with open(txt_file_name, 'w') as f: #write points to text file
+            f.write(line_points1)
+            f.write('\n')
+        
+    x=[]
+    y=[]
+    if  items[i].childNodes.length == 5: #check if there is a second polyline
+        
         if items[i].childNodes.item(3).attributes['label'].value == 'line3':
             line3_points = items[i].childNodes.item(3).attributes['points'].value
             output_line3 = line3_points.replace(';',',')
-            x1, y1, x2, y2, x3, y3 = map(float, output_line3.split(',') )
+            gListB= output_line3.split(',')
 
         elif items[i].childNodes.item(3).attributes['label'].value == 'line2':
             line2_points = items[i].childNodes.item(3).attributes['points'].value
             output_line2 = line2_points.replace(';',',')
-            x1, y1, x2, y2, x3, y3 = map(float, output_line2.split(',') )
+            gListB= output_line2.split(',')
 
-    coordinates2=[]
-    for j in range(590, 300, -10):
-          if j>=y2 and j<=y1 :
-              m, b= func(x1,y1,x2,y2)
-              x_desired=(j-b)/m
-              coordinates2.append((x_desired,j))
-          elif j>=y3 and j<=y2 :
-              m, b= func(x2,y2,x3,y3)
-              x_desired=(j-b)/m
-              coordinates2.append((x_desired,j)) 
+    
+    x,y = func2(gListB)
+    coordinates2=finding_piecewise_equations(x,y)
+    #print(coordinates2)
+
+
     line_points2 = ' '.join(map(str,coordinates2))
     line_points2 = line_points2.replace(') (',',')
     line_points2 = line_points2.replace(')','')
